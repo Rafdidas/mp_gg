3bf3f21cb46a9c0e8ceb4b54051f52f0d05b1910
99uminji <rnalswl96@naver.com>
2025-01-16 20:52:51 +0900

    Docs: .env 파일에 base url 추가

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,13 @@ import Home from "./routes/home/home.component";
 import Main from "./routes/main/main.component";
 
 const App: FC = () => {
-  
-
   return (
     <Routes>
       <Route path="/" element={<Home />}>
-        <Route path="/" element={<Main />}/>
+        <Route path="/" element={<Main />} />
       </Route>
     </Routes>
-  )
-}
+  );
+};
 
 export default App;
--- src/routes/main/main.component.tsx
+++ src/routes/main/main.component.tsx
@@ -1,78 +1,73 @@
-import { FC, useEffect, useState } from 'react';
-import './main.style.scss';
+import { FC, useEffect, useState } from "react";
+import "./main.style.scss";
 
 interface OverallRanking {
-    date: string;
-    ranking: number;
-    character_name: string;
-    world_name: string;
-    class_name: string;
-    sub_class_name: string;
-    character_level: number;
-    character_exp: number;
-    character_popularity: number;
-    character_guildname: string;
+  date: string;
+  ranking: number;
+  character_name: string;
+  world_name: string;
+  class_name: string;
+  sub_class_name: string;
+  character_level: number;
+  character_exp: number;
+  character_popularity: number;
+  character_guildname: string;
 }
 
 const Main: FC = () => {
-    const API_KEY = process.env.REACT_APP_MAPLE_KEY;
-    // const characterName = "CHARACTER NAME";
-    const urlString =
-        "https://open.api.nexon.com/maplestory/v1/ranking/overall?date=2023-12-22&page=1";
+  const API_KEY = process.env.REACT_APP_MAPLE_KEY;
+  const BASE_URL = process.env.REACT_APP_BASE_URL;
+  // const characterName = "CHARACTER NAME";
+  const urlString = `${BASE_URL}/ranking/overall?date=2023-12-22&page=1`;
 
+  const [overallRanking, setOverallRanking] = useState<OverallRanking[]>([]);
+  // const [error, setError] = useState<string | null>(null);
 
-    const [overallRanking, setOverallRanking] = useState<OverallRanking[]>([]);
-    // const [error, setError] = useState<string | null>(null);
+  useEffect(() => {
+    const overallFetch = async () => {
+      try {
+        const response = await fetch(urlString, {
+          headers: {
+            "x-nxopen-api-key": API_KEY || "",
+          },
+        });
+        if (!response.ok) {
+          throw new Error(`error: ${response.status}`);
+        }
 
-    useEffect(() => {
-        const overallFetch = async () => {
-            try {
-                const response = await fetch(urlString, {
-                    headers: {
-                        "x-nxopen-api-key": API_KEY || "",
-                    },
-                });
-                if (!response.ok) {
-                    throw new Error(`error: ${response.status}`);
-                }
+        const data = await response.json();
+        setOverallRanking(data.ranking);
+        console.log(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-                const data = await response.json();
-                setOverallRanking(data.ranking);
-                console.log(data);
-            } catch (error) {
-                console.error(error);
-            }
-        };
+    overallFetch();
+  }, [urlString, API_KEY]);
 
-        overallFetch();
-        
-    }, [urlString, API_KEY]);
-
-    return (
-        <section id='main'>
-            <h1>Main</h1>
-            <ul>
-            {
-                overallRanking.map((rank) => {
-                    return (
-                        <li key={rank.ranking} style={{borderBottom: '1px solid #aaa'}}>
-                            
-                            <p>{rank.ranking}</p>
-                            <p>{rank.character_name}</p>
-                            <p>{rank.world_name}</p>
-                            <p>{rank.class_name}</p>
-                            <p>{rank.sub_class_name}</p>
-                            <p>{rank.character_level}</p>
-                            <p>{rank.character_exp}</p>
-                            <p>{rank.character_popularity}</p>
-                            <p>{rank.character_guildname}</p>
-                        </li>
-                    )
-                })
-            }
-            </ul>
-        </section>
-    )
-}
+  return (
+    <section id="main">
+      <h1>Main</h1>
+      <ul>
+        {overallRanking.map((rank) => {
+          return (
+            <li key={rank.ranking} style={{ borderBottom: "1px solid #aaa" }}>
+              <p>{rank.ranking}</p>
+              <p>{rank.character_name}</p>
+              <p>{rank.world_name}</p>
+              <p>{rank.class_name}</p>
+              <p>{rank.sub_class_name}</p>
+              <p>{rank.character_level}</p>
+              <p>{rank.character_exp}</p>
+              <p>{rank.character_popularity}</p>
+              <p>{rank.character_guildname}</p>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
+};
 
 export default Main;
