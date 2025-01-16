ac0bb7cba9d969221cf4cfb59664d7874d3590ea
99uminji <rnalswl96@naver.com>
2025-01-16 21:57:08 +0900

    Fix: 메인에서 fetch 하도록 수정정

--- src/components/ranking/overall.tsx
+++ src/components/ranking/overall.tsx
@@ -1,50 +1,12 @@
-import { useEffect, useState } from "react";
+import { FC } from "react";
+import { OverallRanking } from "../../types/ranking.types";
 import OverallItem from "./overall-item";
-import { getTodayDate } from "../../utils/getTodayDate.utils";
 
-interface OverallRanking {
-  date: string;
-  ranking: number;
-  character_name: string;
-  world_name: string;
-  class_name: string;
-  sub_class_name: string;
-  character_level: number;
-  character_exp: number;
-  character_popularity: number;
-  character_guildname: string;
+interface OverallProps {
+  overallRanking: OverallRanking[];
 }
 
-const API_KEY = process.env.REACT_APP_MAPLE_KEY;
-const BASE_URL = process.env.REACT_APP_BASE_URL;
-const Overall = () => {
-  const todayDate = getTodayDate();
-  const urlString = `${BASE_URL}/ranking/overall?date=${todayDate}&page=1`;
-  const [overallRanking, setOverallRanking] = useState<OverallRanking[]>([]);
-
-  useEffect(() => {
-    const overallFetch = async () => {
-      try {
-        const response = await fetch(urlString, {
-          headers: {
-            "x-nxopen-api-key": API_KEY || "",
-          },
-        });
-        if (!response.ok) {
-          throw new Error(`error: ${response.status}`);
-        }
-
-        const data = await response.json();
-        setOverallRanking(data.ranking);
-        console.log(data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    overallFetch();
-  }, [urlString, API_KEY]);
-
+const Overall: FC<OverallProps> = ({ overallRanking }) => {
   return (
     <div>
       <h2>종합 랭킹</h2>
--- src/routes/main/main.component.tsx
+++ src/routes/main/main.component.tsx
@@ -1,27 +1,70 @@
 import { FC, useEffect, useState } from "react";
-import "./main.style.scss";
+import { getTodayDate } from "../../utils/getTodayDate.utils";
+import { OverallRanking, UnionRanking } from "../../types/ranking.types";
 import Overall from "../../components/ranking/overall";
+import Union from "../../components/ranking/union";
+import "./main.style.scss";
 
-interface OverallRanking {
-  date: string;
-  ranking: number;
-  character_name: string;
-  world_name: string;
-  class_name: string;
-  sub_class_name: string;
-  character_level: number;
-  character_exp: number;
-  character_popularity: number;
-  character_guildname: string;
-}
+const API_KEY = process.env.REACT_APP_MAPLE_KEY;
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 const Main: FC = () => {
+  const todayDate = getTodayDate();
+  const urlString = `${BASE_URL}/ranking/overall?date=${todayDate}&page=1`;
+  const [overallRanking, setOverallRanking] = useState<OverallRanking[]>([]);
+  const [unionRanking, setUnionRanking] = useState<UnionRanking[]>([]);
+
+  useEffect(() => {
+    const overallFetch = async () => {
+      try {
+        const response = await fetch(urlString, {
+          headers: {
+            "x-nxopen-api-key": API_KEY || "",
+          },
+        });
+        if (!response.ok) {
+          throw new Error(`error: ${response.status}`);
+        }
+
+        const data = await response.json();
+        setOverallRanking(data.ranking);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    const unionFetch = async () => {
+      try {
+        const response = await fetch(urlString, {
+          headers: {
+            "x-nxopen-api-key": API_KEY || "",
+          },
+        });
+        if (!response.ok) {
+          throw new Error(`error: ${response.status}`);
+        }
+
+        const data = await response.json();
+        setUnionRanking(data.ranking);
+        console.log(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    overallFetch();
+    unionFetch();
+  }, [urlString]);
+
   return (
     <section id="main">
       <h1>Main</h1>
       <ul>
         <li>
-          <Overall />
+          <Overall overallRanking={overallRanking} />
+        </li>
+        <li>
+          <Union unionRanking={unionRanking} />
         </li>
       </ul>
     </section>
