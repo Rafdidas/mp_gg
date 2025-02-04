a0d2221a3748d8da39f535c3be6b460eec07e7ff
99uminji <rnalswl96@naver.com>
2025-02-04 16:46:22 +0900

    Docs: header 마크업 적용

--- src/index.tsx
+++ src/index.tsx
@@ -5,8 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import { QueryClientProvider } from "@tanstack/react-query";
 import reportWebVitals from "./reportWebVitals";
 import queryClient from "./api/client";
-import "./index.scss";
-
+import "./styles/reset.scss";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
@@ -20,7 +19,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
